427979046997adf0823a9f216466ad499c74746c
smudgegame <atmargraff@gmail.com>
2021-03-12 21:21:30 -0500

    initial commit

---
+++ src/test/java/org/smudge/petstore/PetstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smudge.petstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
