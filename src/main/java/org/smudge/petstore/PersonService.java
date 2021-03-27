32f45a9c7de325d35d3a3f467fee6ceb81240b15
smudgegame <atmargraff@gmail.com>
2021-03-26 20:28:04 -0400

    Add get person feature

--- src/main/java/org/smudge/petstore/PersonService.java
+++ src/main/java/org/smudge/petstore/PersonService.java
@@ -3,37 +3,20 @@ package org.smudge.petstore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 public class PersonService {
-    //Person id to person
-    private Map<Integer, Person> persons = new HashMap<>();
 
     @Autowired
     private PersonRepository repo;
 
-    public PersonService() {
-        this.persons.put(0, new Person(0, "Aaron"));
-        this.persons.put(1, new Person(1, "Austin"));
-        this.persons.put(2, new Person(2, "Jennifer"));
-    }
-
     public Person getPerson(int id) {
-        return persons.get(id);
+        return repo.findById(id).get().toBusinessObject();
     }
 
     public Person createPerson(String name) {
-        PersonEntity newPerson = new PersonEntity(getNextKey(), name);
+        PersonEntity newPerson = new PersonEntity(0, name);
         repo.save(newPerson);
-        //  this.persons.put(newPerson.getId(), newPerson);
         return newPerson.toBusinessObject();
-    }
-
-    private int getNextKey() {
-        return Collections.max(this.persons.keySet()) + 1;
     }
 
     public Person changeName(int id, String newName) {
